feafe3e14c7ad4ef1b16d5d5e700f202b456238b
Andrew Martinek <andrewdmartinek@gmail.com>
2022-05-02 14:01:47 -0500

    working on category-seeds

---
+++ seeds/category-seeds.js
@@ -0,0 +1,41 @@
+const { Category } = require('../models');
+
+const categoryData = [
+  {
+    category_name: 'Solid',
+  },
+  {
+    category_name: 'Savory',
+  },
+  {
+    category_name: 'Drink',
+  },
+  {
+    category_name: 'Milk',
+  },
+  {
+    category_name: 'Soup',
+  },
+  {
+    category_name: 'Tea',
+  },
+  {
+    category_name: 'Sweet',
+  },
+  {
+    category_name: 'Sour',
+  },
+  {
+    category_name: 'Spicy',
+  },
+  {
+    category_name: 'Lie',
+  },
+  {
+    category_name: 'Wine',
+  },
+];
+
+const seedCategories = () => Category.bulkCreate(categoryData);
+
+module.exports = seedCategories;
