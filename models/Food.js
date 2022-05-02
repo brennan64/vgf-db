2b507bb1b501f0fdc511596a43f6f1000cd817e5
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-05-02 11:44:30 -0500

    food model completed

--- models/Food.js
+++ models/Food.js
@@ -1,3 +1,50 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connections');
+const Login = require('./Login');
+
+
+class Food extends Model { }
+
+Food.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    dish_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 35],
+        isAlpha: true,
+      },
+    },
+    dish_game: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
+    dish_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+          isAlpha: true,
+      },
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'food',
+  }
+);
+
+module.exports = Food;
 
