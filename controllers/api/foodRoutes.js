8447c03eca139261a706d2faa2d762ebc036b058
Brennan <brennanheley@gmail.com>
2022-05-02 13:30:24 -0500

    added test route

--- controllers/api/foodRoutes.js
+++ controllers/api/foodRoutes.js
@@ -3,27 +3,24 @@ const Food = require("../../models/Food");
 
 // GET all foods
 router.get("/", async (req, res) => {
-  try {
-    const foodData = awafood.findAll({});
-    // console.log(studentData);
-    const plainFoodData = foodData.map((food) => food.get({ plain: true }));
-    res.render("homepage", { Food: foodData });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  const foodData = await Food.findAll().catch((err) => {
+    res.json(err);
+  });
+  const foodss = foodData.map((dish) => dish.get({ plain: true }));
+  res.render("all", { foods });
 });
 
-router.post("/", async (req, res) => {
-  try {
-    const foodData = await Food.create({
-      dish_name: req.body.dish_name,
-      dish_game: req.body.dish_game,
-      dish_type: req.dish_type,
-    });
-    res.status(200).json(foodData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+// router.post("/", async (req, res) => {
+//   try {
+//     const foodData = await Food.create({
+//       dish_name: req.body.dish_name,
+//       dish_game: req.body.dish_game,
+//       dish_type: req.dish_type,
+//     });
+//     res.status(200).json(foodData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 module.exports = router;
