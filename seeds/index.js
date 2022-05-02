3f5827c1c0e165dc4045af543d3f6baf3dd1ae01
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-05-02 11:43:58 -0500

    seed data for index and vgf layout

--- seeds/index.js
+++ seeds/index.js
@@ -8,17 +8,9 @@ const sequelize = require('../config/connection');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
-  await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
 
-  await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
-
-  await seedTags();
-  console.log('\n----- TAGS SEEDED -----\n');
-
-  await seedProductTags();
-  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+  await vgfSeed();
+  console.log('\n----- VGF SEEDED -----\n');
 
   process.exit(0);
 };
