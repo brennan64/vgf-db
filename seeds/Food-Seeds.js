9f5d97f4cd29cedfc51487bbe080a8454b74769e
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-05-03 10:21:30 -0500

    fixed the rest of the single/double apostraphe conflicts.

--- seeds/Food-Seeds.js
+++ seeds/Food-Seeds.js
@@ -19,7 +19,7 @@ const foodData = [
   {
     game_name: 'Witcher III',
     dish_type: 'Drink, Wine',
-    dish_name: 'Guillaume du Bois’ Soulful Wine',
+    dish_name: "Guillaume du Bois' Soulful Wine",
   },
   {
     game_name: 'Witcher III',
@@ -64,7 +64,7 @@ const foodData = [
   {
     game_name: 'Witcher III',
     dish_type: 'Drink, Liquor',
-    dish_name: 'Victor Monnart’s Cognac',
+    dish_name: "Victor Monnart's Cognac",
   },
   {
     game_name: 'Legends of Zelda Oracle of Seasons',
