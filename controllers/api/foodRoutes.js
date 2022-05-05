300e8bdd9f961156e8278967a49a94638e9a8d1b
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-05-05 14:04:30 -0500

    saving

--- controllers/api/foodRoutes.js
+++ controllers/api/foodRoutes.js
@@ -25,24 +25,4 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.delete("/:id", async (req, res) => {
-  try {
-    const foodData = await Project.destroy({
-      where: {
-        id: req.params.id,
-        // user_id: req.session.user_id,
-      },
-    });
-
-    if (!foodData) {
-      res.status(404).json({ message: "No project found with this id!" });
-      return;
-    }
-
-    res.status(200).json(foodData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 module.exports = router;
